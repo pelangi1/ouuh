15b5642a43f84ab9817d1940f40698bf5b9c8092
keen eyes <56186677+pelangi1@users.noreply.github.com>
2022-04-03 05:40:39 +0700

    Create index.php

---
+++ soyaa/index.php
@@ -0,0 +1,8 @@
+<?php 
+    echo "<h1>hello ges </h1>";
+    echo "<br>ini adalah domain : "; 
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>Lokasi terletak pada : "; 
+    echo $_SERVER['DOCUMENT_ROOT'];
+    
+?>
